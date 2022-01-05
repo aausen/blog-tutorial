b6f500eeebcf4f8ca84cbbdcc1d600b848e1c0de
aausen <ausen.annie@gmail.com>
2022-01-05 15:00:13 -0600

    added show.html.erb

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,10 +1,14 @@
 class ArticlesController < ApplicationController
   def index
-    @articles = Articles.all
+    @articles = Article.all
   end
   def test
     @article = Article.last
     @article.update(title: "viewed 1 time")
     @header = "This is the header"
+  end
+
+  def show
+    @articles = Article.find(params[:id])
   end
 end
